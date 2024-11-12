fb5d263d65ed40a19b22ca5da9198015e270ebfe
monu-1920 <155940063+monu-1920@users.noreply.github.com>
2024-11-12 15:11:27 +0530

    Add files via upload

---
+++ monu2.py
@@ -0,0 +1,8 @@
+# Simple Python program that asks for the user's name and age
+
+# Get user input
+name = input("What is your name? ")
+age = input("How old are you? ")
+
+# Display a message
+print(f"Hello, {name}! You are {age} years old.")
